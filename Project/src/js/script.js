3904decc459e3c5d314a1ab85fff1c88ecba6733
Aidana <kairova-94@mail.ru>
2019-12-12 00:28:16 +0600

    Это наш первый комит

---
+++ Project/src/js/script.js
@@ -0,0 +1,29 @@
+
+
+// var number = 2;
+// var string = " Hello";
+// var boolean = true;
+// var obj = {};
+
+// var persone = {
+//     name: "John",
+//     age: 28,
+//     isMarried: false
+// };
+// let arr = ['plum.png', 'orange.png', 'apple.bmp'];
+
+// console.log(persone.name + string);
+// let answer = prompt("Есть ли вам 18 лет?", "Да");
+// console.log(answer);
+// console.log(arr[0]);
+
+// let incr = 10;
+// let decr = 5;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(10%3);
+
+let isChecked = true;
+let isWatched = false;
+
+console.log(isChecked || isWatched);
