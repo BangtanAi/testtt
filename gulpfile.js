8c076d7296c893137c8fc433aa5ec78b1e5d2834
Aidana <kairova-94@mail.ru>
2019-12-12 20:08:59 +0600

    Это наш четвертый комит

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concatCSS    = require('gulp-concat-css');
+// var ftp          = require('gulp-ftp');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "src/"
+    });
+    //Следим за изменениями файлов
+    gulp.watch("src/sass/*.sass", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/js/*.js").on('change', browserSync.reload);
+});
+
+// компилируем Sass в Css и обновляем страницу
+gulp.task('sass', function() {
+    return gulp.src("src/sass/*.sass")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+        				browsers: ['last 2 versions'],
+        				cascade: false
+        }))
+        .pipe(concatCSS('style.css'))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// // JavaScript
+// gulp.task('scripts', function(){
+//   return gulp.src("src/js/*.js")
+//     .pipe(js())
+//     .pipe(gulp.dest('js'))
+//     .pipe(reload({stream:true}));
+// });
+//Js files
+
+
+// gulp.task('ftp', function(){
+// 	return gulp.src('src/**')
+// 	.pipe(ftp({
+// 		   host: 'website.com',
+// 		   user: 'johndoe',
+// 		   pass: '1234',
+// 		   remotePath: 'www/islamov.ru/stream'
+// 	}))
+// 	.pipe(gutil.noop());
+// })
+
+gulp.task('default', ['serve']);
---
+++ src/js/script.js
@@ -0,0 +1,99 @@
+var money;
+var time;
+var expens;
+var moneyExpens;
+var everyDay;
+var expenses;
+var appData = {
+    budjet: 0,
+    timeData: '',
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+function checkMoney() {
+    var money = prompt('Ваш бюджет на месяц?');
+    everyDay = money;
+    var time = prompt('Введите дату в формате YYYY-MM-DD');
+if (money&&money > 0) {
+    appData.budjet = money;
+    appData.time = time;
+    console.log('Ваш бюджет на месяц ' + appData.budjet);
+} else {
+    alert('Данные введены не верно. Бюджет на месяц должен быть в числовом формате');
+checkMoney();
+}
+}
+checkMoney();
+ 
+function checkExpens(){
+    expens = prompt('Введите обязательную статью расходов в этом месяце');
+    moneyExpens = prompt('Во сколько обойдётся');
+    console.log(expens, moneyExpens);
+}
+checkExpens();
+console.log('Бюджет на день ' + everyDay/30);
+
+// var money = prompt('Ваш бюджет на месяц?');
+// var time = prompt('Введите дату в формате YYYY-MM-DD');
+// console.log(money);
+// console.log(time);
+
+
+// var number = 2;
+// var string = " Hello";
+// var boolean = true;
+// var obj = {};
+
+// var persone = {
+//     name: "John",
+//     age: 28,
+//     isMarried: false
+// };
+// let arr = ['plum.png', 'orange.png', 'apple.bmp'];
+
+// console.log(persone.name + string);
+// let answer = prompt("Есть ли вам 18 лет?", "Да");
+// console.log(answer);
+// console.log(arr[0]);
+
+// let incr = 10;
+// let decr = 5;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(10%3);
+
+// let isChecked = true;
+// let isWatched = false;
+
+// console.log(isChecked || isWatched);
+
+// if(3*2 == 7){
+//  console.log("Верно!");   
+// } else {
+//     console.log("Неверно!");
+// }
+
+// let num = 130;
+// if (num < 40) {
+//     console.log('Неверно!');
+// } else if (num > 100) {
+//     console.log('Много!');
+// } else if (num == 50) {
+//     console.log('Верно!');
+// }
+// let num = 50;
+// switch (num) {
+//     case num < 49:
+//         console.log('Неверно!');
+//         break;
+//     case num > 100:
+//         console.log('Верно!');
+//         break;
+//     case num > 80:
+//         console.log('Все еще верно!');
+//         break;
+//     default:
+//         console.log('Что-то пошло не так');
+// }
